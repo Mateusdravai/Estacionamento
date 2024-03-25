a37048ed0f97f16529b8514209d08f2df998893c
mateus <mateusulisses2000@gmail.com>
2024-03-25 01:03:40 -0300

    Refatoração: Refatoração de vários métodos e correção de bugs

--- Estacionamento/Estacionamento.cs
+++ Estacionamento/Estacionamento.cs
@@ -1,15 +1,140 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Estacionamento
+﻿namespace Estacionamento
 {
     public class Estacionamento
     {
         public required List<int> VagasPequenas { get; set; }
         public required List<int> VagasMedias { get; set; }
         public required List<int> VagasGrandes { get; set; }
+
+        public bool Disponibilidade()
+        {
+
+            int vagasVan = 0;
+            int vagasDisponiveisMoto = (VagasPequenas.Capacity - VagasPequenas.Count) + (VagasGrandes.Capacity + VagasGrandes.Count) + (VagasMedias.Capacity - VagasMedias.Count);
+            int vagasDisponiveisCarro = VagasMedias.Capacity - VagasMedias.Count + (VagasGrandes.Capacity + VagasGrandes.Count);
+            for (int i = 1; i <= VagasMedias.Capacity - VagasMedias.Count; i++)
+            {
+                if ((i % 3) == 0)
+                    vagasVan = vagasVan + 1;
+            }
+            int vagasDisponiveisVan = (VagasGrandes.Capacity - VagasGrandes.Count) + vagasVan;
+
+            if (vagasDisponiveisMoto == 0)
+            {
+                Console.WriteLine("Não temos mais vagas de moto disponíveis neste momento.");
+                return false;
+            }
+
+            if (vagasDisponiveisCarro == 0)
+            {
+                Console.WriteLine("Não temos mais vagas disponíveis para carros ou vans neste momento.");
+                return false;
+            }
+
+            if (vagasDisponiveisVan == 0)
+            {
+                Console.WriteLine("Não temos mais vagas disponíveis neste momento.");
+                return false;
+            }
+
+            return true;
+        }
+
+        public void EstacionarVeiculo(TipoVeiculo veiculo)
+        {
+            switch (veiculo)
+            {
+                case TipoVeiculo.Moto:
+                    if (VagasPequenas.Count < VagasPequenas.Capacity)
+                    {
+                        VagasPequenas.Add((int)veiculo);
+                        Console.WriteLine("Vaga registrada, bem vindo(a)!");
+                    }
+                    else if (VagasGrandes.Count < VagasGrandes.Capacity)
+                    {
+                        VagasGrandes.Add((int)veiculo);
+                        Console.WriteLine("Vaga registrada, bem vindo(a)!");
+                    }
+                    else if (VagasMedias.Count < VagasMedias.Capacity)
+                    {
+                        VagasMedias.Add((int)veiculo);
+                        Console.WriteLine("Vaga registrada, bem vindo(a)!");
+                    }
+                    Console.WriteLine("Todas as vagas de moto estão ocupadas.");
+                    break;
+
+                case TipoVeiculo.Carro:
+                    if (VagasMedias.Count < VagasMedias.Capacity)
+                    {
+                        VagasMedias.Add((int)veiculo);
+                        Console.WriteLine("Vaga registrada, bem vindo!");
+                    }
+                    else if (VagasGrandes.Count < VagasGrandes.Capacity)
+                    {
+                        VagasGrandes.Add((int)veiculo);
+                        Console.WriteLine("Vaga registrada, bem vindo!");
+                    }
+                    Console.WriteLine("Todas as vagas de carro estão ocupadas.");
+                    break;
+
+                case TipoVeiculo.Van:
+                    if (VagasGrandes.Count < VagasGrandes.Capacity)
+                    {
+                        VagasGrandes.Add((int)veiculo);
+                        Console.WriteLine("Vaga registrada, bem vindo!");
+                    }
+                    else if ((VagasMedias.Capacity - VagasMedias.Count) >= 3)
+                    {
+                        VagasMedias.Add((int)veiculo);
+                        VagasMedias.Add((int)veiculo);
+                        VagasMedias.Add((int)veiculo);
+                        Console.WriteLine("Vaga registrada, bem vindo!");
+                    }
+                    Console.WriteLine("Todas as vagas de van estão ocupadas.");
+                    break;
+            }
+        }
+
+
+        public void EstadoAtualVagas()
+        {
+            int vagasTotal, restanteMoto, restanteCarro, restanteVan, vagasVan = 0;
+
+            int vagasDisponiveisMoto = (VagasPequenas.Capacity - VagasPequenas.Count) +
+                (VagasGrandes.Capacity - VagasGrandes.Count) +
+                (VagasMedias.Capacity - VagasMedias.Count);
+
+            int vagasDisponiveisCarro = (VagasMedias.Capacity - VagasMedias.Count) +
+                (VagasGrandes.Capacity - VagasGrandes.Count);
+
+            for (int i = 1; i <= VagasMedias.Capacity - VagasMedias.Count; i++)
+            {
+                if ((i % 3) == 0)
+                    vagasVan = vagasVan + 1;
+            }
+
+            int vagasDisponiveisVan = (VagasGrandes.Capacity - VagasGrandes.Count) + vagasVan;
+
+            vagasTotal = VagasPequenas.Capacity + VagasMedias.Capacity + VagasGrandes.Capacity;
+
+            restanteMoto = VagasPequenas.Where(x => x == (int)TipoVeiculo.Moto).Count() +
+                VagasMedias.Where(x => x == (int)TipoVeiculo.Moto).Count() +
+                VagasGrandes.Where(x => x == (int)TipoVeiculo.Moto).Count();
+
+            restanteCarro = VagasMedias.Where(x => x == (int)TipoVeiculo.Carro).Count() +
+                VagasGrandes.Where(x => x == (int)TipoVeiculo.Carro).Count();
+
+            restanteVan = VagasMedias.Where(x => x == (int)TipoVeiculo.Van).Count() +
+                (VagasGrandes.Where(x => x == (int)TipoVeiculo.Van).Count() / 3);
+
+            Console.WriteLine(@$"Total simples de vagas restantes {vagasTotal}
+                {vagasDisponiveisMoto} vagas restantes para motos
+                {restanteMoto} vagas ocupadas por motos
+                {vagasDisponiveisCarro} vagas restantes para carro
+                {restanteCarro} vagas ocupadas por carros
+                {vagasDisponiveisVan} vagas restantes para van.
+                {restanteVan} vagas ocupadas por carros");
+        }
+
     }
 }
--- Estacionamento/Program.cs
+++ Estacionamento/Program.cs
@@ -5,18 +5,19 @@
         static void Main(string[] args)
         {
             #region variaveis
-            int opcao = -1, vagasMoto, vagasCarro, vagasVan, vagasTotal, restanteMoto, restanteCarro, restanteVan;
+
             string? nome;
+            TipoVeiculo veiculo;
             Estacionamento estacionamento = new Estacionamento()
             {
                 VagasPequenas = new List<int>(3),
                 VagasGrandes = new List<int>(3),
                 VagasMedias = new List<int>(3)
             };
-            EstacionamentoRepositorio estacionamentoRepositorio = new EstacionamentoRepositorio();
+
             #endregion
 
-            while (opcao != 0)
+            while (true)
             {
                 do
                 {
@@ -27,118 +28,32 @@
 
                 Console.WriteLine($"Bem vindo ao Estacionamento Savassi {nome}!");
 
-                if (!estacionamentoRepositorio.Disponibilidade(estacionamento))
+                if (!estacionamento.Disponibilidade())
                 {
-                    Console.WriteLine("Estacionamento cheio no momento.");
+                    Console.WriteLine("Estacionamento cheios no momento.");
                     return;
                 }
 
-                vagasMoto = estacionamentoRepositorio.RetornarVagasMoto(estacionamento.VagasPequenas.Capacity,
-                    estacionamento.VagasPequenas.Count);
-                vagasCarro = estacionamentoRepositorio.RetornarVagasCarro(estacionamento.VagasMedias.Capacity,
-                    estacionamento.VagasMedias.Count);
-                vagasVan = estacionamentoRepositorio.RetornarVagasVan(estacionamento.VagasGrandes.Capacity,
-                    estacionamento.VagasGrandes.Count);
-
-                vagasTotal = vagasMoto + vagasVan + vagasCarro;
-
-                vagasMoto = vagasTotal;
-                vagasCarro = vagasCarro + vagasVan;
-                for (int i = 1; i <= (vagasCarro - vagasVan); i++)
-                {
-                    if (i % 3 == 0)
-                    {
-                        vagasVan += 1;
-                    }
-                }
-
-                 restanteMoto = estacionamento.VagasPequenas.Where(x => x == 1).Count() +
-                     estacionamento.VagasMedias.Where(x => x == 1).Count() +
-                     estacionamento.VagasGrandes.Where(x => x == 1).Count();
-
-                 restanteCarro = estacionamento.VagasMedias.Where(x => x == 2).Count() +
-                     estacionamento.VagasGrandes.Where(x => x == 2).Count();
-
-                 restanteVan = estacionamento.VagasMedias.Where(x => x == 3).Count() +
-                     estacionamento.VagasGrandes.Where(x => x == 3).Count();
-
-                Console.WriteLine($"Total simples de vagas restantes {vagasTotal}\n\n" +
-                    $"{vagasMoto} vagas restantes para motos.\n" +
-                    $"{restanteMoto} vagas ocupadas por motos\n\n" +
-                    $"{vagasCarro} vagas restantes para carro.\n" +
-                    $"{restanteCarro} vagas ocupadas por carros\n\n" +
-                    $"{vagasVan} vagas restantes para van.\n\n" +
-                    $"{restanteVan} vagas ocupadas por carros\n\n");
+                estacionamento.EstadoAtualVagas();
 
                 do
                 {
-                    Console.WriteLine(@"Informe o número correspondente ao tipo de veículo que deseja estacionar:\n" +
-                        "1: Moto\n" +
-                        "2: Carro\n" +
-                        "3: Van\n" +
-                        "0: Sair"
+                    Console.WriteLine(@"Informe o número correspondente ao tipo de veículo que deseja estacionar:\n
+                        0: Moto
+                        1: Carro
+                        2: Van
+                        3: Sair"
                         );
                 }
-                while (int.TryParse(Console.ReadLine(), out opcao) || opcao > 3 || opcao < 0);
+                while (!Enum.TryParse(Console.ReadLine(), out veiculo));
 
-                switch (opcao)
+                if (veiculo == TipoVeiculo.Sair)
                 {
-                    case 1:
-                        if(vagasMoto > 0)
-                        {
-                            estacionamento.VagasPequenas.Add(opcao);
-                            Console.WriteLine("Vaga registrada, bem vindo(a)!");
-                        }
-
-                        else if(vagasCarro > 0)
-                        {
-                            estacionamento.VagasMedias.Add(opcao);
-                            Console.WriteLine("Vaga registrada, bem vindo(a)!");
-                        }
-
-                        else if(vagasVan > 0)
-                        {
-                            estacionamento.VagasGrandes.Add(opcao);
-                            Console.WriteLine("Vaga registrada, bem vindo!");
-                        }
-
-                        Console.WriteLine("Todas as vagas de moto estão ocupadas.");
-                        break;
-
-                    case 2:
-                        if (vagasCarro > 0)
-                        {
-                            estacionamento.VagasMedias.Add(opcao);
-                            Console.WriteLine("Vaga registrada, bem vindo!");
-                        }
-
-                        else if(vagasVan > 0)
-                        {
-                            estacionamento.VagasGrandes.Add(opcao);
-                            Console.WriteLine("Vaga registrada, bem vindo!");
-                        }
-
-                        Console.WriteLine("Todas as vagas de carro estão ocupadas.");
-                        break;
-
-                    case 3:
-                        if(vagasVan > 0)
-                        {
-                            estacionamento.VagasGrandes.Add(opcao);
-                            Console.WriteLine("Vaga registrada, bem vindo!");
-                        }
-
-                        else if(vagasCarro >= 3)
-                        {
-                            estacionamento.VagasMedias.Add(opcao);
-                            estacionamento.VagasMedias.Add(opcao);
-                            estacionamento.VagasMedias.Add(opcao);
-                            Console.WriteLine("Vaga registrada, bem vindo!");
-                        }
-
-                        Console.WriteLine("Todas as vagas de van estão ocupadas.");
-                        break;
+                    Console.WriteLine("Obrigado pelo seu tempo conosco, volte sempre!");
+                    return;
                 }
+
+                estacionamento.EstacionarVeiculo(veiculo);
             }
         }
     }
